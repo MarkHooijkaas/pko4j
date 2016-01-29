eb17132c472b73713e0a3753598beb9eaac448f2
Mark Hooijkaas <mark@hooijkaas.net>
2016-01-29 14:31:39 +0100

    do the actual git commit asynchronously in a separate thread

--- src/main/java/org/kisst/pko4j/impl/FileStorage.java
+++ src/main/java/org/kisst/pko4j/impl/FileStorage.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.eclipse.jgit.api.Git;
 import org.kisst.item4j.json.JsonOutputter;
 import org.kisst.item4j.json.JsonParser;
 import org.kisst.item4j.seq.ArraySequence;
@@ -14,39 +13,41 @@ import org.kisst.item4j.seq.TypedSequence;
 import org.kisst.item4j.struct.MultiStruct;
 import org.kisst.item4j.struct.SingleItemStruct;
 import org.kisst.item4j.struct.Struct;
-import org.kisst.pko4j.PkoObject;
 import org.kisst.pko4j.PkoModel;
+import org.kisst.pko4j.PkoObject;
 import org.kisst.pko4j.PkoSchema;
 import org.kisst.pko4j.StructStorage;
+import org.kisst.pko4j.impl.GitStorage.Commit;
 import org.kisst.props4j.Props;
 import org.kisst.util.CallInfo;
 import org.kisst.util.FileUtil;
 
 public class FileStorage<T extends PkoObject> implements StructStorage<T> {
 	public final PkoSchema<T> schema;
+	private final String domain="@wikivents.nl";
 
 	private final File dir;
 	private final String name;
 	private final JsonParser parser=new JsonParser();
 	private final JsonOutputter outputter = new JsonOutputter(null);
 	private final Class<T> cls;
-	private final Git git;
+	private final GitStorage git;
 	
 	//private final Repository gitrepo;
 
-
+	
+	
 	@SuppressWarnings("unchecked")
-	public FileStorage(PkoSchema<T> schema, Git git, File maindir) {
+	public FileStorage(PkoSchema<T> schema, GitStorage git, File maindir) {
 		this.schema=schema;
 		this.cls=(Class<T>) schema.getJavaClass();
 		this.name=cls.getSimpleName();
 		dir=new File(maindir,name);
 		if (! dir.exists())
 			dir.mkdirs();
-		//loadAllRecords();
 		this.git=git;
 	}
-	public FileStorage(PkoSchema<T> schema, Props props, Git git) {
+	public FileStorage(PkoSchema<T> schema, Props props, GitStorage git) {
 		this(schema, git, new File(props.getString("datadir", "data"))); 
 	}
 	
@@ -56,14 +57,9 @@ public class FileStorage<T extends PkoObject> implements StructStorage<T> {
 		String key = value.getKey();
 		if (key==null)
 			key=createUniqueKey();
-		File f = getFile(key);
-		if (f.exists())
-			throw new RuntimeException("File "+f.getAbsolutePath()+" already exists");
-		File dir = getDir(key);
-		if (! dir.exists())
-			dir.mkdirs();
-		FileUtil.saveString(f, outputter.createString(value));
-		gitCommit("create"+name,key);
+		createCommit("create", value)
+			.newFile(getFile(value), outputter.createString(value))
+			.enqueue();
 	}
 	
 	private static AtomicInteger number=new AtomicInteger(new Random().nextInt(13));
@@ -77,36 +73,28 @@ public class FileStorage<T extends PkoObject> implements StructStorage<T> {
 				parser.parse(f),
 				new SingleItemStruct("savedModificationDate", Instant.ofEpochMilli(f.lastModified()))
 				);
-		//System.out.println(result);
 		return result;
 	}
 	
-	//@Override public T read(String key) {
-	//	File f = getFile(key);
-	//	return createStruct(f);
-	//}
 	
 	@Override public void update(T oldValue, T newValue) {
 		// The newValue may contain an id, but that is ignored
-		String oldId = oldValue.getKey();
-		File f = getFile(oldId);
-		FileUtil.saveString(f, outputter.createString(newValue));
-		//git.add().addFilepattern(name+"/"+f.getName()).call();
-		gitCommit("update"+name,oldId);
+		createCommit("update "+name,oldValue)
+			.changeFile(getFile(oldValue), outputter.createString(newValue))
+			.enqueue();
 	}
 	@Override public void delete(T oldValue)  {
 		checkForConcurrentModification(oldValue);
-		getFile(oldValue).delete();
-		gitCommit("delete"+name,oldValue.getKey());
+		createCommit("delete ",oldValue)
+			.deleteFile(getFile(oldValue))
+			.enqueue();
 	}
 	private void checkForConcurrentModification(T obj) {
 		// TODO Auto-generated method stub
 
 	}
-	private File getDir(String key) { return new File(dir, key+".dir"); }
 	private File getFile(String key, String path) { return new File(dir, key+".dir/"+path); }
-	private File getFile(String key) { return getFile(key, "record.dat"); }
-	private File getFile(T obj) { return getFile(obj.getKey());}
+	private File getFile(T value) { return getFile(value.getKey(), "record.dat"); }
 
 	@Override public TypedSequence<T> findAll(PkoModel model) {
 		ArrayList<T> list=new ArrayList<>();
@@ -118,11 +106,20 @@ public class FileStorage<T extends PkoObject> implements StructStorage<T> {
 				String key=f.getName();
 				if (! (key.endsWith(".dir") && f.isDirectory()))
 					continue;
-				f=new File(f,"record.dat");
+				if (new File(dir,"deleted").exists())
+					continue;
+				File newest=null;
+				for (File f3 : f.listFiles()) {
+					if (f3.getName().equals("record.dat") || f3.getName().endsWith(".todo")) {
+						if (newest==null)
+							newest=f3;
+						else if (f3.getName().compareTo(newest.getName())>0)
+							newest=f3;
+					}
+				}
 				count++;
 				key=key.substring(0,key.length()-4);
-
-				Struct doc=createStruct(f);
+				Struct doc=createStruct(newest);
 				list.add(schema.createObject(model, doc));
 			}
 			catch (Exception e) { e.printStackTrace();}// TODO: return dummy placeholder
@@ -131,20 +128,16 @@ public class FileStorage<T extends PkoObject> implements StructStorage<T> {
 		return new ArraySequence<T>(cls,list);
 	}
 
-	private void gitCommit(String action, String data) {
+	private Commit createCommit (String action, T value) {
 		try {
-			if (git==null)
-				return;
+			String data = value.getName();
 			CallInfo callinfo = CallInfo.instance.get();
 			if (callinfo.action!=null)
 				action=callinfo.action;
 			if (callinfo.data!=null)
 				data=callinfo.data;
-			String comment = action+" by "+callinfo.user+" on "+data;
-			synchronized(git) {				
-				git.add().addFilepattern(".").call();
-				git.commit().setMessage(comment).call();
-			}
+			String comment = action+" on "+data;
+			return git.createCommit("ip:"+callinfo.ip, callinfo.user+domain ,comment);
 		}
 		catch (Exception e) { throw new RuntimeException(e); }
 	}
---
+++ src/main/java/org/kisst/pko4j/impl/GitStorage.java
@@ -0,0 +1,110 @@
+package org.kisst.pko4j.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.kisst.item4j.struct.ReflectStruct;
+import org.kisst.util.FileUtil;
+
+public class GitStorage implements Runnable {
+	private final Git git;
+	private final ArrayBlockingQueue<Commit> queue=new ArrayBlockingQueue<>(10);
+	
+	private AtomicLong counter = new AtomicLong(System.currentTimeMillis());
+
+	
+	public final class Commit extends ReflectStruct {
+		private class FileChange {
+			public final File file;
+			protected FileChange(File f) { this.file=f; }
+			public FileChange(File f, String content) { 
+				this.file=f;
+				File file= todoFile();
+				File dir = file.getParentFile();
+				if (! dir.exists())
+					dir.mkdirs();
+				FileUtil.saveString(file , content);
+			}
+			protected File todoFile() { return new File(file.getAbsolutePath()+"."+id+".todo"); }
+			public void prepareCommit() { 
+				if (file.exists())
+					file.delete();
+				todoFile().renameTo(file);
+			}
+		}
+		private class AppendToFile extends FileChange {
+			public final String content;
+			public AppendToFile(File f, String content) { super(f); this.content=content; }
+			public void prepareCommit() { 
+				FileUtil.appendString(file, content);
+			}
+		}
+		private class DeleteFile extends FileChange {
+			public DeleteFile(File f) { 
+				super(f);
+				//File f = ,"deleted");
+				//FileUtil.saveString(f, "deleted");
+			}
+			public void prepareCommit() { 
+				file.delete();
+			}
+		}
+		
+		public final String id;
+		public final String user;
+		public final String mail;		
+		public final String comment;
+		public final ArrayList<FileChange> changes=new ArrayList<>();;
+		public Commit(String user, String mail,String comment) {
+			this.id=""+counter.getAndIncrement();
+			this.user=user;
+			this.mail=mail;
+			this.comment=comment;
+		}
+		public Commit newFile(File filename, String content) { changes.add(new FileChange(filename,content)); return this; } // TODO: check file does not yet exist
+		public Commit changeFile(File filename, String content) { changes.add(new FileChange(filename,content)); return this; }
+		public Commit appendToFile(File filename, String content) { changes.add(new AppendToFile(filename,content)); return this; }
+		public Commit deleteFile(File filename) { changes.add(new DeleteFile(filename)); return this; }
+		
+		
+		public void enqueue() { queue.add(this); }
+		private void commit() {
+			synchronized(git) {				
+				try {
+					for (FileChange c: changes) {
+						c.prepareCommit();
+						git.add().addFilepattern(c.file.getAbsolutePath());
+					}
+					//git.add().addFilepattern(".").call();
+					git.commit().setAuthor(user,mail).setMessage(comment).call();
+				}
+				catch (GitAPIException e) { throw new RuntimeException(e); }
+			}
+		}
+	}
+
+	public void run() {
+		while (true) {
+			Commit c;
+			try {
+				c = queue.take();
+			}
+			catch (InterruptedException e) {  break; }
+			c.commit();
+		}
+	}
+	public GitStorage(File dir) {
+		try {
+			git = Git.open(dir);
+			new Thread(this).start();
+		}
+		catch (IOException e) { throw new RuntimeException(e);}
+	}
+
+	public Commit createCommit(String user, String ip,String comment) { return new Commit(user,ip,comment); }
+}
